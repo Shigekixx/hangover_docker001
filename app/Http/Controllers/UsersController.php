69b13fced0c181418c495210e43b8b80af98b297
Shigeki Shizawa <s.shizawa@so-ra.co.jp>
2024-05-20 16:54:08 +0900

    マイページにログインしたアカウント名が表示される・１日の記録の投稿フォーム・機能実装完了

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UserRequest;
+use App\Http\Requests\DiaryRequest;
 use App\Models\User;//Userモデルを使用するために追加
+use App\Models\Diary;//Userモデルを使用するために追加
 
 class UsersController extends Controller
 {
@@ -31,6 +34,28 @@ class UsersController extends Controller
     //マイページを表示
     public function mypage()
     {   
-        return view('mypage.mypage');
+        $id = Auth::id();
+        $user = DB::table('users')->find($id);
+        return view('mypage.mypage',['user_info'=>$user]);
+    }
+
+    //マイページにて新規記録の入力
+    public function diary(DiaryRequest $request)
+    {
+        $diary = new Diary;
+        $diary->user_id = auth()->id();
+        $diary->sleep = $request->sleep;
+        $diary->tired = $request->tired;
+        $diary->drink = $request->drink;
+        $diary->hangover = $request->hangover;
+        $diary->memo = $request->memo;
+        if ($request->hasFile('photo')) {
+            $diary->photo = $request->file('photo')->store('public');
+        } else {
+            $diary->photo = null;
+        }
+
+        $diary->save();
+        return redirect()->route('mypage.mypage');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ Route::POST('/login',[LoginController::class,'login'])->name('login.login');
 
 Route::get('/users',[UsersController::class,'showregister'])->name('users.showregister');
 Route::POST('/users',[UsersController::class,'register'])->name('users.register');
+
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/mypage',[UsersController::class,'mypage'])->name('mypage.mypage');
-    });
+    Route::POST('/mypage',[UsersController::class,'diary'])->name('mypage.diary');
+});
